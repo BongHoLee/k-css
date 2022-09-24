bcd224cc4d47e79151af8718d9f7e4a5cc7f8a10
bongholee <leebongho9204@gmail.com>
2022-09-25 00:30:06 +0900

    update

---
+++ src/main/java/com/kcss/kcss/KCssApplication.java
@@ -0,0 +1,13 @@
+package com.kcss.kcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KCssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcss/kcss/KCssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcss.kcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KCssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
